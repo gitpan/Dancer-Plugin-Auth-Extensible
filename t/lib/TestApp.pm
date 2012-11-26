68fd90ced456cd2275289a331e95aa3ec5df4204
David Precious <davidp@preshweb.co.uk>
2012-11-26 20:00:50 +0000

    Import of BIGPRESH/Dancer-Plugin-Auth-Extensible-0.01 from CPAN.
    gitpan-cpan-distribution: Dancer-Plugin-Auth-Extensible
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         BIGPRESH/Dancer-Plugin-Auth-Extensible-0.01.tar.gz
    gitpan-cpan-author:       BIGPRESH
    gitpan-cpan-maturity:     released

---
+++ t/lib/TestApp.pm
@@ -0,0 +1,44 @@
+package t::lib::TestApp;
+
+use Dancer;
+
+
+
+set session => 'simple';
+set plugins => { 'Auth::Extensible' => { provider => 'Example' } };
+
+use Dancer::Plugin::Auth::Extensible;
+no warnings 'uninitialized';
+
+
+get '/' => sub {
+    "Index always accessible";
+};
+
+get '/loggedin' => sub :RequireLogin {
+    "You are logged in";
+};
+
+get '/name' => sub :RequireLogin {
+    return "Hello, " . logged_in_user->{name};
+};
+
+get '/roles' => sub :RequireLogin {
+    return join ',', sort @{ user_roles() };
+};
+
+get '/realm' => sub :RequireLogin {
+    return session->{logged_in_user_realm};
+};
+
+get '/beer' => sub :RequireRole(BeerDrinker) {
+    "You can have a beer";
+};
+
+get '/piss' => sub :RequireRole(BearGrylls) {
+    "You can drink piss";
+};
+
+
+
+1;
